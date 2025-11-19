a2eb48bba579287e9b6c198adea9e1ba20628fd8
Igor <igoroleksiewicz9@gmail.com>
2025-11-19 09:29:34 +0100

    labty

--- main.py
+++ main.py
@@ -1,43 +1,38 @@
-# =================================  TESTY  ===================================
-# Testy do tego pliku zostały podzielone na dwie kategorie:
-#
-#  1. `..._invalid_input`:
-#     - Sprawdzające poprawną obsługę nieprawidłowych danych wejściowych.
-#
-#  2. `..._correct_solution`:
-#     - Weryfikujące poprawność wyników dla prawidłowych danych wejściowych.
-# =============================================================================
 import numpy as np
 import scipy as sp
 
 
 def is_diagonally_dominant(A: np.ndarray | sp.sparse.csc_array) -> bool | None:
-    """Funkcja sprawdzająca czy podana macierz jest diagonalnie zdominowana.
-
-    Args:
-        A (np.ndarray | sp.sparse.csc_array): Macierz A (m,m) podlegająca 
-            weryfikacji.
+    if not isinstance(A, (np.ndarray, sp.sparse.csc_array)):
+        return None
     
-    Returns:
-        (bool): `True`, jeśli macierz jest diagonalnie zdominowana, 
-            w przeciwnym wypadku `False`.
-        Jeżeli dane wejściowe są niepoprawne funkcja zwraca `None`.
-    """
-    pass
-
-
-def residual_norm(A: np.ndarray, x: np.ndarray, b: np.ndarray) -> float | None:
-    """Funkcja obliczająca normę residuum dla równania postaci: 
-    Ax = b.
-
-    Args:
-        A (np.ndarray): Macierz A (m,n) zawierająca współczynniki równania.
-        x (np.ndarray): Wektor x (n,) zawierający rozwiązania równania.
-        b (np.ndarray): Wektor b (m,) zawierający współczynniki po prawej 
-            stronie równania.
+    if A.ndim != 2 or A.shape[0] != A.shape[1]:
+        return None  
     
-    Returns:
-        (float): Wartość normy residuum dla podanych parametrów.
-        Jeżeli dane wejściowe są niepoprawne funkcja zwraca `None`.
-    """
-    pass
+    if len(A.shape) != 2:
+        return None
+    
+    diag = np.diagonal(A)
+    
+    for i in range(A.shape[0]):
+        row_sum = np.sum(np.abs(A[i, :])) - np.abs(diag[i])
+        if np.abs(diag[i]) <= row_sum:
+            return False
+    return True
+
+def residual_norm(A: np.ndarray, x: np.ndarray, b: np.ndarray):
+    if not isinstance(A, np.ndarray) or not isinstance(x, np.ndarray) or not isinstance(b, np.ndarray):
+        return None
+    
+    if A.shape[1] != x.shape[0]:
+        return None  
+    if A.shape[0] != b.shape[0]:
+        return None  
+
+    if x.ndim != 1 or b.ndim != 1:
+        return None
+    
+    residual = b - A.dot(x)
+    norm = np.linalg.norm(residual)
+    
+    return norm
