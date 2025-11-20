8db261804fa8965fc469fcf0e22ccd9ebcb584c7
Igur7 <igoroleksiewicz9@gmail.com>
2025-11-20 10:25:39 +0100

    commit dom

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 import numpy as np
 import scipy as sp
-
+from scipy.sparse import issparse
 
 def is_diagonally_dominant(A: np.ndarray | sp.sparse.csc_array) -> bool | None:
+
     if not isinstance(A, (np.ndarray, sp.sparse.csc_array)):
         return None
     
-    if A.ndim != 2 or A.shape[0] != A.shape[1]:
-        return None  
-    
-    if len(A.shape) != 2:
+    if A.ndim !=2:
         return None
     
-    diag = np.diagonal(A)
-    
-    for i in range(A.shape[0]):
-        row_sum = np.sum(np.abs(A[i, :])) - np.abs(diag[i])
-        if np.abs(diag[i]) <= row_sum:
-            return False
-    return True
+    if A.shape[0] != A.shape[1]:
+        return None
+
+    if issparse(A):
+        A = A.toarray()
+
+    diag_elements = np.abs(np.diagonal(A))
+
+    row_sums = np.sum(np.abs(A), axis=1) - diag_elements
+
+    return np.all(diag_elements > row_sums)
 
 def residual_norm(A: np.ndarray, x: np.ndarray, b: np.ndarray):
     if not isinstance(A, np.ndarray) or not isinstance(x, np.ndarray) or not isinstance(b, np.ndarray):
